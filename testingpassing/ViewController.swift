185a1025993e5abf9875497ac8f8ed314d622d7e
ToTo1403 <ditasuhito@gmail.com>
2023-10-26 18:56:43 +0700

    Initial Commit

---
+++ testingpassing/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  testingpassing
+//
+//  Created by ToTo  on 26/10/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
