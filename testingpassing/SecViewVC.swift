81c7c58dc735a4b58d1f6495d78142381f8b3947
ToTo1403 <ditasuhito@gmail.com>
2024-02-10 13:15:50 +0700

    testing

--- testingpassing/SecViewVC.swift
+++ testingpassing/SecViewVC.swift
@@ -6,3 +6,26 @@
 //
 
 import Foundation
+import UIKit
+class SecViewVC : UIViewController,passDataDelegate {
+    func passdata(text: String) {
+        carrydata = text
+        print(carrydata,"carrydata")
+        if let destinationVC = storyboard?.instantiateViewController(withIdentifier: "ViewController") as? ViewController {
+                destinationVC.delegate = self
+                navigationController?.pushViewController(destinationVC, animated: true)
+            }
+    }
+    var carrydata = ""
+    var carrydatalabl = ""
+    @IBOutlet weak var secTextField: UITextField!
+    @IBOutlet weak var secLB: UILabel!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    override func viewWillAppear(_ animated: Bool) {
+        secLB.text = carrydata
+        secTextField.text = carrydatalabl
+        
+    }
+}
--- testingpassing/ViewController.swift
+++ testingpassing/ViewController.swift
@@ -6,14 +6,30 @@
 //
 
 import UIKit
+protocol passDataDelegate : AnyObject {
+    func passdata(text: String)
+}
+class ViewController: UIViewController, UITextFieldDelegate {
 
-class ViewController: UIViewController {
-
+    @IBOutlet weak var firstTextField: UITextField!
+    @IBOutlet weak var firstlb: UILabel!
+    weak var delegate : passDataDelegate?
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        firstTextField.delegate = self
+        
     }
 
-
+    @IBAction func gonext(_ sender: Any) {
+        performSegue(withIdentifier: "goto", sender: nil)
+       
+    }
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "goto", let secvc = segue.destination as? SecViewVC{
+            secvc.carrydata = firstTextField.text ?? ""
+            secvc.carrydatalabl = firstlb.text ?? ""
+        }
+    }
 }
 
